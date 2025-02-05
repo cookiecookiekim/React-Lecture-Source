a117f11f1f8c0721c7fe9afe3eb057f15546f13c
cookiecookiekim <kimkunam06@gmail.com>
2025-02-05 13:17:58 +0900

    :wrench: 동일한 이름의 모듈 css 파일 만들고 연결하기

---
+++ 05_router/03_params/src/pages/About.js
@@ -0,0 +1,12 @@
+/* 소개 페이지 */
+/* /menu 요청이 들어오면 보여줄 페이지 */
+
+function About(){
+    return(
+        <div>
+            <h1>(About)하이미디어 소개 페이지.</h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ 05_router/03_params/src/pages/Menu.js
@@ -0,0 +1,15 @@
+/* 메뉴들을 보여주는 페이지 */
+/* /menu 요청이 들어오면 보여줄 페이지 */
+
+function Menu(){
+    return(
+        <>
+            <h1>Menu 페이지 전체 메뉴 조회</h1>
+            <div className={boxStyle.MenuBox}>
+
+            </div>
+        </>
+    );
+}
+
+export default Menu;
