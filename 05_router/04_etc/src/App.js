a73b31a537fccb54f5ea2e7c4bb6fc39cfc87134
cookiecookiekim <kimkunam06@gmail.com>
2025-02-06 10:23:59 +0900

    :wrench:  예외 처리하기 시작

---
+++ 05_router/04_etc/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter , Route, Routes} from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Main from './pages/Main';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Error404 from './pages/errors/Error404';
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Main/>}/>
+        <Route path='main' element={<Main/>}/>
+        <Route path='menu' element={<Menu/>}/>
+        <Route path='about' element={<About/>}/>
+      </Route>
+      <Route path='*' element={<Error404/>}/>
+     </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ 05_router/04_etc/src/pages/errors/Error404.js
@@ -0,0 +1,9 @@
+function Error404(){
+    return(
+        <>
+            <h1>404 NOT FOUND 에러 발생</h1>
+        </>
+    );
+}
+
+export default Error404;
